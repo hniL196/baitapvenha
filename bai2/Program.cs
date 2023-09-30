530f9f87bf79e4cbb9183008f4a37b63aff1524a
Admin <Admin@DESKTOP-ERN2T0A>
2023-09-30 21:16:39 +0700

    bai lab 2-1

---
+++ bai2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class bai2
+{
+    public static void Main(string[] args)
+    {
+        int a;
+
+        Console.WriteLine("Nhap so bat ky: ");
+        a = Convert.ToInt16(Console.ReadLine());
+
+        TimeSpan result = TimeSpan.FromSeconds(a);
+        string fromTimeString = string.Format("{0:D2} Gio {1:D2} Phut {2:D2} Giay",result.Hours, result.Minutes,result.Seconds);
+
+        Console.WriteLine(fromTimeString);
+    }
+}
--- baitaplab2/Program.cs
+++ baitaplab2/Program.cs
@@ -5,6 +5,54 @@
         int nam;
         int thang;
 
-        
+        Console.Write("Nhap so nam: ");
+        nam = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Nhap so thang: ");
+        thang = Convert.ToInt16(Console.ReadLine());
+
+        switch (thang)
+        {
+            case 1:
+                Console.WriteLine("31 ngay");
+                break;
+            case 2:
+                if (nam % 4 == 0)
+                    if (nam % 100 != 0)
+                        Console.WriteLine("29 ngay");
+                    else
+                        Console.WriteLine("28 ngay");
+                break;
+            case 3:
+                Console.WriteLine("31 ngay");
+                break;
+            case 4:
+                Console.WriteLine("30 ngay");
+                break;
+            case 5:
+                Console.WriteLine("31 ngay");
+                break;
+            case 6:
+                Console.WriteLine("30 ngay");
+                break;
+            case 7:
+                Console.WriteLine("31 ngay");
+                break;
+            case 8:
+                Console.WriteLine("31 ngay");
+                break;
+            case 9:
+                Console.WriteLine("30 ngay");
+                break;
+            case 10:
+                Console.WriteLine("31 ngay");
+                break;
+            case 11:
+                Console.WriteLine("30 ngay");
+                break;
+            case 12:
+                Console.WriteLine("31 ngay");
+                break;
+        }
     }
 }
