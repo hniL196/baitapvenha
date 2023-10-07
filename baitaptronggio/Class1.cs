a7513ccb0a885b461b207c1a5b2a4db93b08d1d3
Admin <Admin@DESKTOP-ERN2T0A>
2023-10-07 19:39:05 +0700

    a

---
+++ baitaptronggio/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitaptronggio
+{
+    public class Class1
+    {
+        public string name;
+        public int age;
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public void Display (string name, int age)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }       
+    }
+}
--- baitaptronggio/Program.cs
+++ baitaptronggio/Program.cs
@@ -4,42 +4,13 @@
 // tìm kiếm xem số k có trong mảng a hay không
 // nếu có, in ra vị trí của phần tử có gia trị là k
 // nếu không in ra không có
+using baitaptronggio;
+using System.Security.Cryptography.X509Certificates;
+
 public class clas
 {
     public static void Main(string[] args)
     {
-        // Khai báo sô phần tử trong mảng
-        int n;
-        Console.WriteLine("Nhap so phan tu trong mang");
-        n = Convert.ToInt32(Console.ReadLine());
-
-        int[] a = new int[n];
-
-        //Duyệt mảng để thêm giá trị từng vị trí trong mảng a
-        for (int i = 0; i < a.Length; i++)
-        {
-            Console.WriteLine($"Nhap gia tri cua phan tu thu {i} trong mang a:");
-            a[i] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        int k;
-        Console.WriteLine("Nhap so bat ki");
-        k = Convert.ToInt32(Console.ReadLine());
-        int check = 0;
-
-        // Duyệt mảng tìm giá tri
-        for (int j = 0; j < a.Length; j++)
-        {
-            if (k == a[j])
-            {
-                check = 1;
-                Console.WriteLine($"{k} co trong mang tai vi tri {j + 1}");
-            }
-        }
-
-        if( check == 0)
-        {
-            Console.WriteLine($"{k} khong co trong mang");
-        }
+        
     }
 }
